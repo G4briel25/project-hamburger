250317d1268858b96541421d8dbc51e5af1b4d6b
G4briel25 <warmerishoth@gmail.com>
2024-05-20 21:24:58 -0400

    base url editado

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://G4briel25.github.io/project-hamburger/',
+  base: '/project-hamburger/',
   plugins: [
     vue(),
   ],
